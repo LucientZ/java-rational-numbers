e37807f9f4da876f6cffa4f6b1d347ffa1048c99
LucientZ <koblenzpoisiden@gmail.com>
2023-11-11 19:13:28 -0600

    Created initial test and methods

--- csce331-testing-rational/src/main/java/numbers/Rational.java
+++ csce331-testing-rational/src/main/java/numbers/Rational.java
@@ -6,8 +6,24 @@ package numbers;
  */
 public class Rational 
 {
+    private int numerator;
+    private int denominator;
+
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+
+    public Rational(){
+        this.numerator = 0;
+        this.denominator = 1;
+    }
+
+    public int numerator(){
+        return this.numerator;
+    }
+
+    public int denominator(){
+        return this.denominator;
     }
 }
--- csce331-testing-rational/src/test/java/numbers/RationalTest.java
+++ csce331-testing-rational/src/test/java/numbers/RationalTest.java
@@ -1,5 +1,8 @@
 package numbers;
 
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
@@ -7,32 +10,33 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple Rational.
  */
-public class RationalTest 
-    extends TestCase
-{
+public class RationalTest
+        extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public RationalTest( String testName )
-    {
-        super( testName );
+    public RationalTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( RationalTest.class );
+    public static Test suite() {
+        return new TestSuite(RationalTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testRational()
-    {
-        assertTrue( true );
+    public void testRational() {
+        // Given no preconditions
+
+        Rational value = new Rational();
+
+        assertThat("The numerator is 0", value.numerator(), is(0));
+        assertThat("The denominator is 1", value.denominator(), is(1));
     }
 }
